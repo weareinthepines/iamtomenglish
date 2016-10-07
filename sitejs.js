668620c5db3d91df97e52cda09c43c18c42517c5
weareinthepines <thomaswenglish@gmail.com>
2016-10-07 14:30:46 -0400

    newest site

--- sitejs.js
+++ sitejs.js
@@ -1,2 +1,12 @@
-$(document).ready(function(){       
+$(document).ready(function(){    
+	var x = new Date ();
+		var y = x.getHours();
+		if (y > 6 || y < 12)
+			$("#parallax").css("background-image", "url('sunrise.png')");
+		else if (y > 12 || y < 18)
+			$("#parallax").css("background-image", "url('noon.jpg')");
+		else if (y > 18 || y < 24)
+			$("#parallax").css("background-image", "url('sunset.jpg')");
+		else
+			$("#parallax").css("background-image", "url('midnight.jpg')");
 });
